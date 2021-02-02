46f4d54d01f2decb6432b85a3f13f59d98a6571c
Salome-Chevailler <74154174+Salome-Chevailler@users.noreply.github.com>
2021-02-02 15:22:31 +0100

    Partie 2 : infos sur un Pokemon

--- pokemon.js
+++ pokemon.js
@@ -23,7 +23,8 @@ function getPokemon(event) {
       // ajouter une <option value="url du pok">name du pok</option> pour chaque pokemon
       let texteHTML = "";
       for (let p of pokemons) {
-        texteHTML = texteHTML + "<option value = p.url>" + p.name + "</option>";
+        texteHTML =
+          texteHTML + '<option value = "' + p.url + '">' + p.name + "</option>";
       }
       // pour l'affichage dans le navigateur
       document.getElementById("liste").innerHTML = texteHTML;
@@ -36,3 +37,47 @@ function getPokemon(event) {
 
 // Appel de la fonction
 getPokemon();
+
+// ====== Afficher la description d'un Pokemon choisi dans la liste =====
+// -- gestion de la sélection
+// cible : le liste d'id "liste"
+// event : change
+// action : description du Pokemon
+document.getElementById("liste").addEventListener("change", getDescription);
+
+// ====== la fonction qui récupère la description dy Pokemon et l'affiche
+function getDescription(event) {
+  const url = event.target.value; // l’url de la ressource
+  console.log(url);
+  let fetchOptions = { method: "GET" }; // les options de l'API fetch
+
+  // exécuter la requête AJAX
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataJSON) => {
+      // dataJSON = les données renvoyées
+      console.log(dataJSON);
+      let description = dataJSON;
+
+      // ici le traitement des données …
+      let texteHTML = "";
+
+      texteHTML =
+        "Le pokemon " +
+        description.name +
+        " mesure " +
+        description.height +
+        " cm et pèse " +
+        description.weight +
+        " kg";
+
+      // pour l'affichage dans le navigateur
+      document.getElementById("detail").innerHTML = texteHTML;
+    })
+    .catch((error) => {
+      // gestion des erreurs
+      console.log(error);
+    });
+}
